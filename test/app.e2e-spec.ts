83a6afdf9ec46c20b2d26757541f1d063b8d2b91
Bohdan <bohdan_heniuk@epam.com>
2023-07-26 12:31:52 +0300

    commit

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,14 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from '../src/fibonacci.module';
+import { FibonacciModule } from '../src/fibonacci.module';
 
-describe('AppController (e2e)', () => {
+describe('Fibonacci Controller (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [FibonacciModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -16,9 +15,6 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return expect(app).toBeDefined();
   });
 });
