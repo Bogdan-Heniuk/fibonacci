a0f8f2e3d1f1285dae10ea55e59dae60fa637ca0
Bohdan <bohdan_heniuk@epam.com>
2023-07-26 12:48:54 +0300

    updated 2

--- src/fibonacci.controller.ts
+++ src/fibonacci.controller.ts
@@ -10,8 +10,8 @@ export class FibonacciController {
   constructor(private readonly fibonacciService: FibonacciService) {}
 
   @MessagePattern({ cmd: 'calc_fibonacci' })
-  async getHello(@Payload() index: string): Promise<any> {
+  async getHello(@Payload() index: string): Promise<number> {
     const result = await this.fibonacciService.calcFibonacci(Number(index));
-    return result + " updated!";
+    return result;
   }
 }
